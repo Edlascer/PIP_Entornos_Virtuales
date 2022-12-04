8f517f91ff20356517dc82b8fb4bc3472f84a8df
Edrei Hernandez <edlascer@LAPTOP-0J6379E7.localdomain>
2022-12-04 00:24:46 -0600

    Modificacion a la lectura del csv con Pandas

--- app/main.py
+++ app/main.py
@@ -1,17 +1,38 @@
 import utils
 import read_csv
 import charts
+import pandas as pd #Se importa pandas como pd (convencionalidad)
 
 def run():
-  data = read_csv.read_csv('data.csv')
+  
+  '''
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
-  
+  '''
+
+  '''
+  Pandas se encarga de leer csv, filtrar columnas, datos etc. Aunque es importante
+  conocer como podemos resolver el problema sin un módulo.
+  '''
+
+  #Dataframes en Pandas son todos los datos de donde se obtiene información
+  # normalmente se definen con df
+   
+  df = pd.read_csv('data.csv') #Nos ahorramos el código del archivo read_csv
+  df = df[df['Continent'] == 'Africa'] #Sustitución al filter
+
+  #Obtener los valores de las columnas con Pandas
+  countries = df['Country'].values
+  percentages = df['World Population Percentage'].values
+  charts.generate_pie_chart(countries, percentages)
+
+  data = read_csv.read_csv('data.csv') #Dejamos esta línea para no corromper el código de abajo
   country = input('Type Country => ')
 
+  #Código proveniente de utils.py
   result = utils.population_by_country(data, country)
 
   if len(result) > 0:
