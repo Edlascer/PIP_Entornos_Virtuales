fd1a8a4ed804fb1686b4a0e3a44046bb5d906014
Edrei Hernandez <edlascer@LAPTOP-0J6379E7>
2022-12-04 23:01:18 -0600

    Servidor web con FastAPI y Uvicorn

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,15 @@
 import store
+from fastapi import FastAPI
+
+app = FastAPI() #Instancia de la aplicación
+
+@app.get('/') #Slash o ruta donde podrán acceder desde la web
+def get_list():
+    return[1, 2, 3]
+
+@app.get('/contact') #Slash o ruta donde podrán acceder desde la web
+def get_list():
+    return{'name' : 'Platzi'}
 
 def run():
     store.get_categories()
